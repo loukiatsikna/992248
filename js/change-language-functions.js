9f8a3668997321c8b9cfdecd319f5a8fe7b814e1
loukiatsikna <loukiatsikna@gmail.com>
2025-10-29 19:38:02 +0200

    Add files via upload

---
+++ js/change-language-functions.js
@@ -0,0 +1,73 @@
+var languageContent = {
+    greek: {
+      languageBtn: "EL",
+      mainTitle: "Eπίδομα Στέγασης σε Φοιτητές",
+      pageTitle: "Eπίδομα Στέγασης σε Φοιτητές",
+      infoTitle: "Πληροφορίες για την χορήγηση του Στεγαστικού Επιδόματος σε Φοιτητές",
+      subTitle1: "Αυτό το ερωτηματολόγιο μπορεί να σας βοηθήσει να βρείτε αν δικαιούστε να λάβετε το Φοιτητικό Στεγαστικό Επίδομα ",
+      subTitle2: "H συμπλήρωση του ερωτηματολογίου δεν απαιτεί παραπάνω από 10 λεπτά.",
+      subTitle3: "Δεν θα αποθηκεύσουμε ούτε θα μοιραστούμε τις απαντήσεις σας.",
+      backButton: "Πίσω",
+      nextQuestion: "Επόμενη ερώτηση",
+      biggerCursor: "Μεγαλύτερος Δρομέας",
+      bigFontSize: "Μεγάλο Κείμενο",
+      readAloud: "Ανάγνωση",
+      changeContrast: "Αντίθεση",
+      readingMask: "Μάσκα Ανάγνωσης",
+      footerText: "Αυτό το έργο δημιουργήθηκε για τις ανάγκες της εργασίας μας κατά τη διάρκεια των προπτυχιακών μας σπουδών στο Πανεπιστήμιο Μακεδονίας. Η ομάδα μας αποτελείται από 2 φοιτητές της Εφαρμοσμένης Πληροφορικής:",
+      and: "και",
+      student1: "Αμπατζίδου Ελισάβετ",
+      student2: "Δασύρα Ευμορφία Ελπίδα",
+      startBtn:"Ας ξεκινήσουμε",
+      chooseAnswer: "Επιλέξτε την απάντησή σας",
+      oneAnswer: "Μπορείτε να επιλέξετε μόνο μία επιλογή.",
+      errorAn: "Λάθος:",
+      choose: "Πρέπει να επιλέξετε μια απάντηση",
+    },
+    english: {
+      languageBtn: "EN",
+      mainTitle: "Housing Allowance for Students",
+      pageTitle: "Housing Allowance for Students",
+      infoTitle: "Information on the granting of the Student Housing Allowance",
+      subTitle1: "This questionnaire can help you find out if you are eligible to receive the Student Housing Allowance",
+      subTitle2: "Completing the questionnaire should not take more than 10 minutes.",
+      subTitle3: "We will neither store nor share your answers.",
+      backButton: "Βack",
+      nextQuestion: "Next Question",
+      biggerCursor: "Bigger Cursor",
+      bigFontSize:" Big Font Size",
+      readAloud: "Read Aloud",
+      changeContrast:" Change Contrast",
+      readingMask:" Reading Mask",
+      footerText: "This project was created for the needs of our thesis at the University of Macedonia. Our team consists of 2 students of Applied Informatics:",
+      and: "and",
+      student1: "Ampatzidou Elisavet",
+      student2: "Dasyra Evmorfia Elpida",
+      startBtn:"Let's start",
+      chooseAnswer: "Choose your answer",
+      oneAnswer: "You can choose only one option.",
+      errorAn: "Error:",
+      choose: "You must choose one option",
+    }
+};
+  
+// Retrieve the selected language from localStorage or set default to "greek"
+var currentLanguage = localStorage.getItem("preferredLanguage") || "greek";
+
+function toggleLanguage() {
+    currentLanguage = currentLanguage === "greek" ? "english" : "greek";
+    localStorage.setItem("preferredLanguage", currentLanguage);
+    updateContent();
+}
+
+function updateContent() {
+    var components = document.querySelectorAll(".language-component");
+     
+    components.forEach(function (component) {
+        var componentName = component.dataset.component;
+        component.textContent = languageContent[currentLanguage][componentName];
+    });
+}
+
+// Initialize the content based on the selected language
+updateContent();
